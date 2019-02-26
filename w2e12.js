1e8779f410f5d63712ad4a779487547cc74c7c24
wiwid <ignprad@gmail.com>
2019-02-26 13:11:39 +0700

    submitted

--- w2e12.js
+++ w2e12.js
@@ -1,35 +1,19 @@
 function konversiMenit(menit) {
-    if(menit >= 60 && menit < 120){
-      var jam = 1;
-      var menit2 = menit - 60;
-      if(menit2 < 10){
-        menit2 = "0" + menit2
-      }
-      var tampilanJam = jam + ":" + menit2;
-       //console.log(jam + " : " + menit2);
-      return tampilanJam;
-    }
+  // you can only write your code here!
+  var jam = menit
+  var counter = 0
   
-    else if(menit >= 120 ){
-      var jam = 2;
-      var menit2 = menit - 120;
-      if(menit2 < 10){
-        menit2 = "0" + menit2
-      }
-      var tampilanJam = jam + ":" + menit2;
-      //console.log(jam + " : " + menit2);
-      return tampilanJam;
-    }
-  
-    else if(menit < 60 ){
-      var menit2 = 0 + menit;
-      var tampilanJam = "0:" + menit2 ;
-      //console.log(" 0 : " + menit2);
-      return tampilanJam;
-    }
-  
-  
+  while(jam > 59){
+     jam = jam - 60
+   counter += 1
   }
+  if(jam < 10){
+    return counter + ':0' + jam
+  } else {
+    return counter + ':' + jam
+  }
+     
+}
 
   
 console.log(konversiMenit(63)); // 1:03
@@ -37,3 +21,6 @@ console.log(konversiMenit(124)); // 2:04
 console.log(konversiMenit(53)); // 0:53
 console.log(konversiMenit(88)); // 1:28
 console.log(konversiMenit(120)); // 2:00
+
+
+
