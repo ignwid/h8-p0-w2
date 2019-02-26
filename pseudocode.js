37ae6fbb1b39dcf2167a8c993b848a463e385775
wiwid <ignprad@gmail.com>
2019-02-26 13:23:29 +0700

    submitted

---
+++ pseudocode.js
@@ -0,0 +1,36 @@
+// 1. Newton Second Law
+// STORE 'mass' with value equals to 600
+// STORE 'accel' with value equals to 2
+// STORE 'force' without any value
+// CALCULATE 'mass' times 'accel'
+// SET 'force' value with calculation result
+// DISPLAY 'force'
+
+// 2.Tahun Kabisat
+// STORE 'tahun' with any value
+//     IF ('tahun' modulus 4 equals 0 AND 'tahun' modulus 100 equals 1) OR ('tahun' modulus 4 equals 0 AND 'tahun' modulus 100 equals 0 AND 'tahun' modulus 400 equals 0)
+//         DISPLAY 'tahun kabisat'
+//     ELSE
+//         DISPLAY 'bukan tahun kabisat'
+//     END IF
+
+// 3. Laundry Day
+// STORE 'pakaian' with value equals 20
+// STORE 'hitungPakaian' with value equals 0
+// WHILE 'hitung pakaian' less than 20
+//     CALCULATE 'pakaian' minus 1
+//     CALCULATE 'hitungPakaian' add 1
+//     DISPLAY 'pakaian di mesin cuci: ' + hitungPakaian
+// END WHILE
+// DISPLAY 'Total pakaian di mesin cuci: ' + hitung + ' ,mesin siap dinyalakan'
+
+
+// 4. Periksa Kuku
+// STORE an array called 'kuku' filled with 40 strings named 'bersih' or 'kotor'
+// FOR variable 'i' equals 0 to 'i' less than 40
+//     IF kuku[i] equals 'bersih'
+//         DISPLAY 'puji siswa'
+//     ELSE IF kuku[i] equals 'kotor'
+//         DISPLAY 'hukum siswa'
+//     END IF
+// END FOR  
